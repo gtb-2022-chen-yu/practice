3267652e67b65110624f5ee6084a9018abb79631
Feifei-C <865959026@qq.com>
2022-06-14 22:17:28 +0800

    chore: delete default test generated by Gradle

--- app/src/main/java/ass/Demo/App.java
+++ app/src/main/java/ass/Demo/App.java
@@ -1,11 +1,8 @@
 package ass.Demo;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        throw new UnsupportedOperationException();
     }
 }
